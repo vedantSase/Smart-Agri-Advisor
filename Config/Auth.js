da47af9c025a8cdbf77c792085c1aa5373d16ddd
vedantSase <sasevedant5264@gmail.com>
2025-04-12 03:17:46 +0530

    Updated seeions

--- Config/Auth.js
+++ Config/Auth.js
@@ -1,9 +1,28 @@
 module.exports = {
+    // ensureAuthenticated: function (req, res, next) {
+    //     if(req.isAuthenticated()) {
+    //         return next() ;
+    //     }
+    //     req.flash('error_msg', 'Please log in to view this page');
+    //     res.redirect('/user/login_page');
+    // }   
+
+
     ensureAuthenticated: function (req, res, next) {
-        if(req.isAuthenticated()) {
-            return next() ;
+        if (req.isAuthenticated()) {
+            return next();
         }
+    
+        // Initialize a stack if not present
+        if (!req.session.returnToStack) {
+            req.session.returnToStack = [];
+        }
+    
+        // Push current route to stack
+        req.session.returnToStack.push(req.originalUrl);
+    
         req.flash('error_msg', 'Please log in to view this page');
         res.redirect('/user/login_page');
-    }   
+    }
+    
 }
